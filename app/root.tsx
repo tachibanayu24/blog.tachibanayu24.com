5391baa125a332218251fe69fda591a47a326d8e
Yuto Tachibana <tachibanayu24@gmail.com>
2022-10-03 03:41:15 +0900

    Setup environment

--- app/root.tsx
+++ app/root.tsx
@@ -7,16 +7,27 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "たちばなゆうとのブログ",
   viewport: "width=device-width,initial-scale=1",
 });
 
+export function links() {
+  return [
+    { rel: "stylesheet", href: styles },
+    {
+      rel: "icon",
+      href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🐰</text></svg>",
+    },
+  ];
+}
+
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <Meta />
         <Links />
